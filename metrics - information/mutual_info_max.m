f0443da659ac34f2fc3a83a258df84a6ba3836d2
Harang Ju <harangju@gmail.com>
2018-02-21 16:45:27 -0500

    refactor MI max

--- metrics - information/mutual_info_max.m
+++ metrics - information/mutual_info_max.m
@@ -1,12 +1,16 @@
-function [mi_max, time] = mutual_info_max(info, C)
+function [mi_max, node, time] = mutual_info_max(info, C)
 %mutual_info_max 
 %   mi: [N X ] matrix
 %   C: output nodes
 %returns
-%   mi_max: [N X 1] vector of maximum MI values
-%   time: time at which MI is maximum
+%   mi_max: vector of max MI values (excluding zeros)
+%   node: node idx
+%   time: time of mi_max
 
-[mi_max, time] = max(info(find(C), :), [], 2);
+node = find(C);
+[mi_max, time] = max(info(node, :), [], 2);
+node(mi_max<=0) = [];
+time(mi_max<=0) = [];
+mi_max(mi_max<=0) = [];
 
 end
-

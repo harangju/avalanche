e916fdd09f2a1700300849af8d9e8fb00a112d63
Harang Ju <harangju@gmail.com>
2018-02-21 17:16:26 -0500

    shuffle output nodes

--- network/shuffle_output_nodes.m
+++ network/shuffle_output_nodes.m
@@ -1,7 +1,14 @@
-function [outputArg1,outputArg2] = shuffle_output_nodes(N, A)
+function C = shuffle_output_nodes(N, B, num_nodes_output)
 %UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+%   N: number of nodes
+%   B: connectivity for inputs
+%   num_nodes_output
 
+idx = 1:N;
+idx(B>0) = [];
+num_nodes_hidden = N - sum(B>0) - num_nodes_output;
+idx(randperm(length(idx), num_nodes_hidden)) = [];
+C = zeros(N,1);
+C(idx) = 1;
+
+end

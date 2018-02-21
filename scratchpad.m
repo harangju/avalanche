efb5c24832021e76d151bd6e1f8264da2f38f5ed
Harang Ju <harangju@gmail.com>
2018-02-20 19:41:35 -0500

    stuff

--- scratchpad.m
+++ scratchpad.m
@@ -5,9 +5,11 @@
 %% Initialize
 disp('Initializing...')
 p = default_network_parameters;
-p.num_nodes = 10;
-p.num_nodes_input = p.num_nodes;
-p.frac_conn = 4e-1;
+p.num_nodes = 30;
+p.num_nodes_input = 10;
+p.num_nodes_output = 10;
+p.num_nodes_hidden = 10;
+p.frac_conn = 2e-1;
 p.graph_type = 'WRG';
 p.exp_branching = 1;
 [A, B, C] = network_create(p);
@@ -18,9 +20,7 @@ sig = branching(A);
 disp(['avg branching parameter: ' num2str(mean(sig)) '+-' ...
     num2str(std(sig)/p.num_nodes)])
 u_t = zeros(p.num_nodes, 1);
-[~, idx_max_ave_c] = sort(ave_control(A));
-[~, idx_max_mod_c] = sort(modal_control(A));
-u_t(idx_max_ave_c(end-1:end), 1) = 1;
+u_t(find(B,1), 1) = 1;
 Y_t = trigger_avalanche(A, B, u_t, 10);
 clf; plot_summary(A, avalanche_size_analytical(A, B, 5),...
     ave_control(A), modal_control(A), Y_t, avalanche_transitions(Y_t, A))

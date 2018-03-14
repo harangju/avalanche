1c9bd500d8c794ba33a59d24d95561cce3931a48
Harang Ju <harangju@gmail.com>
2018-03-14 16:22:15 -0400

    B should include only connected nodes

--- generate_network.m
+++ generate_network.m
@@ -2,16 +2,16 @@
 %% generate network from data
 load('beggs data/DataSet2.mat')
 A = estimate_network_from_spikes(data, 0.1);
-B = ones(size(A,1),1);
+B = double(outdegree(A)>0);
 A = scale_weights_to_criticality(A);
 beep
 
 %% generate network with random connections
 p = default_network_parameters;
-p.num_nodes = 256;
+p.num_nodes = 443;
 p.num_nodes_input = p.num_nodes;
 p.num_nodes_output = p.num_nodes;
-p.frac_conn = 1/p.num_nodes;
+p.frac_conn = 0.0055;
 p.graph_type = 'WRG';
 p.exp_branching = 1;
 [A, B, C] = network_create(p);

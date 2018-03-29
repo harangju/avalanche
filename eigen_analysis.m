e8c51ae7341743bdcf0653802be161930e77c453
Harang Ju <harangju@gmail.com>
2018-03-29 15:51:52 -0400

    eigen graphs & inputs

--- eigen_analysis.m
+++ eigen_analysis.m
@@ -1,5 +1,6 @@
 
 % given A
+%%
 deg = outdegree(A) + indegree(A);
 A_conn = A(deg>0,deg>0);
 g=graph_from_matrix(A_conn);
@@ -17,7 +18,23 @@ plot(g,'NodeCData',eigenmags,...
     'LineWidth',2*g.Edges.Weight/max(g.Edges.Weight))
 colorbar; set(gca,'visible','off'); axis square
 subplot(1,2,2)
-eigenmags = zeros(size(A_conn,1),1);
-for i = idx'
-eigenmags = max(eigenmags, u(i)*ones(size(eigenmags)));
-end
+plot(g,'NodeCData',modal_control(A_conn),...
+    'MarkerSize',5,...
+    'LineWidth',2*g.Edges.Weight/max(g.Edges.Weight))
+colorbar; set(gca,'visible','off'); axis square
+
+%%
+clf; hold on
+for i = idx(end-10:end)'
+    bar(v(:,i))
+end; hold off
+axis square; prettify
+
+%%
+idx(end-5:end)'
+u(idx(end-5:end))'
+x0=-1*v(:,88); x=[];
+for i=1:10; x=[x A_conn^i*x0]; end;
+plot(x','LineWidth',2); prettify; axis square
+
+
--- inputs/inputs.m
+++ inputs/inputs.m
@@ -1,11 +1,12 @@
-function u = inputs(N, nodes)
-%inputs Generates inputs
+function u = inputs(N, nodes, val)
+%inputs(N, nodes, val) Generates inputs
 %   N: number of nodes
 %   nodes: cell array of vectors of node indices
+%   val: the value of the input
 
 u = zeros(N, length(nodes));
 for t = 1 : length(nodes)
-    u(nodes{t}, t) = 1;
+    u(nodes{t}, t) = val;
 end
 
 end

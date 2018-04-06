f88b7b4040ba70e3bbdb61d4346ed9ad15cea18d
Harang Ju <harangju@gmail.com>
2018-04-06 07:59:13 -0400

    eigenvalue analysis

--- avalanche generation/trigger_many_avalanches.m
+++ avalanche generation/trigger_many_avalanches.m
@@ -1,10 +1,10 @@
-function [Y, pattern_order, sizes] = trigger_many_avalanches(A, B, patterns, ...
-    pattern_probs, max_duration, iterations)
-%trigger_many_avalanches Calls trigger_avalanche many times
+function [Y, pattern_order, sizes] = trigger_many_avalanches(A, B, ...
+    patterns, pattern_probs, max_duration, iterations)
+%trigger_many_avalanches(A,B,patterns,pattern_probs,dur,iter)Calls 
+%trigger_avalanche many times
 %   A:
 %   B:
-%   patterns: cell array of patterns
-%       a pattern is a cell array of nodes to activate across time
+%   patterns: cell array of u_t, {[N X t]}
 %   pattern_probs: vector of probabilities for presenting the i^(th) pattern
 %       sum(u_t_prob) should equal 1
 %   max_duration:
@@ -26,9 +26,9 @@ sizes = zeros(1, iterations);
 for i = 1 : iterations
     pat = sum(int8(prob_cumsum < rand)) + 1;
     pattern_order(i) = pat;
-    u_t = inputs(N, patterns{pat}, 100);
+%     u_t = inputs(N, patterns{pat}, 2);
+    u_t = patterns{pat};
     Y_t = trigger_avalanche(A, B, u_t, max_duration);
-%     Y_t = avalanche_average_analytical(A, B, u_t, max_duration);
     sizes(i) = avalanche_size(Y_t);
     Y(:, 1:size(Y_t,2), i) = Y_t;
 end
--- eigen_analysis.m
+++ eigen_analysis.m
@@ -1,47 +1,74 @@
 
 % given A
-%%
+
+%% create subnetwork
 deg = outdegree(A) + indegree(A);
 A_conn = A(deg>0,deg>0);
 g=graph_from_matrix(A_conn);
+%% eigen analysis
 [v,u] = eig(A_conn);
 u = diag(u);
 [~,idx] = sort(u);
-clf
+%% max eigenvector components
 eigenmags = zeros(size(A_conn,1),1);
 for i = idx'
 eigenmags = max(eigenmags, abs(v(:,i)));
 end
-subplot(1,2,1)
+%% plot graphs
+figure(1)
+subplot(2,1,1)
 plot(g,'NodeCData',eigenmags,...
     'MarkerSize',5,...
     'LineWidth',2*g.Edges.Weight/max(g.Edges.Weight))
 colorbar; set(gca,'visible','off'); axis square
-subplot(1,2,2)
+subplot(2,1,2)
 plot(g,'NodeCData',modal_control(A_conn),...
     'MarkerSize',5,...
     'LineWidth',2*g.Edges.Weight/max(g.Edges.Weight))
 colorbar; set(gca,'visible','off'); axis square
+%% using eigenvectors as stimuli
+
+%% duration as function of eigenvalues
+
+%% mutual information as function of eigenvectors
+
+%% random stimuli
+
+%% duration a.f.o eigenvalues
+
+
+%% mutual information a.f.o. eigenvalues
+
 
 %%
 clf; hold on
 for i = idx(end-10:end)'
-    bar(v(:,i))
+    bar(v(:,i)) 
 end; hold off
 axis square; prettify
+%%
+idx(end-10:end)'
+u(idx(end-10:end))'
 
 %%
-idx(end-5:end)'
-u(idx(end-5:end))'
+
+
+%%
 x0=-1*v(:,88); x=[];
 for i=1:10; x=[x A_conn^i*x0]; end;
 plot(x','LineWidth',2); prettify; axis square
 
 %%
+dur=20; iter=300;
+pat_size = 2;
+idx = {{n(randperm(length(n),pat_size))},...
+        {n(randperm(length(n),pat_size))},...
+        {n(randperm(length(n),pat_size))},...
+        {n(randperm(length(n),pat_size))}...
+        };
+pats = {-1*v(:,idx(end-1)),};
 [Yp,pat] = trigger_many_avalanches(A_conn,ones(size(A_conn,1),1),...
-    {{n(randperm(length(n),2))},...
-        {n(randperm(length(n),2))},...
-        {n(randperm(length(n),2))}}, [1 1 1]/3,10,100);
+    pats, ones(1,length(pats))/length(pats),dur,iter);
 
 
 

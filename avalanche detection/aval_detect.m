ecc54b16b4c1a736b68e4c8a36ab1d1093302d77
Harang Ju <harangju@gmail.com>
2018-05-08 16:37:14 -0400

    avalanche detection

--- avalanche detection/aval_detect.m
+++ avalanche detection/aval_detect.m
@@ -1,6 +1,6 @@
 
 %%
-load('/beggs data/DataSet2.mat')
+load('beggs data/DataSet2.mat')
 %% detect avalanches
 bin_size = 4;
 avalanches = detect_avalanches(data.spikes, bin_size);
@@ -21,4 +21,49 @@ occur_mean = cellfun(@mean,part);
 bar(occur_mean)
 prettify; xlabel('neurons'); ylabel('mean occurrence in avalanche')
 %%
-
+A_raw = estimate_network_from_spikes(data, 0.1);
+beep
+%%
+deg = outdegree(A_raw) + indegree(A_raw);
+A = A_raw(deg>0,deg>0);
+B = ones(1,size(A,1));
+A = scale_weights_to_criticality(A);
+%% check connectivity
+disp(mean(A(:)>0))
+%% eigendecomposition
+[v,d] = eig(A);
+d = diag(d);
+%% get real eigenvectors
+v_real = zeros(params.num_nodes,params.num_nodes);
+d_real = zeros(1,params.num_nodes);
+for i = 1 : params.num_nodes
+    v_real(:,i) = v(:,i);
+    if ~isreal(d(i))
+        if i < params.num_nodes &&...
+                abs(d(i)) == abs(d(i+1))
+            v_real(:,i) = v(:,i) + v(:,i+1);
+            d_real(i) = d(i) + d(i+1);
+        elseif abs(d(i)) == abs(d(i-1))
+            v_real(:,i) = v(:,i) + v(:,i-1);
+            d_real(i) = d(i) + d(i-1);
+        end
+    end
+    if find(v_real(:,i)<0); v_real(:,i) = -1 * v_real(:,i); end
+end; clear i
+%% scale by eigenvalue
+score = max(abs(d .* v),[],2);
+%%
+subplot(2,1,1)
+bar(occur_mean(deg>0))
+subplot(2,1,2)
+bar(score)
+ylabel('score')
+xlabel('neurons')
+set(gca,'FontSize',16);
+subplot(2,1,1)
+ylabel('mean occurrence')
+set(gca,'FontSize',16);
+%%
+scatter(score,occur_mean(deg>0), 'filled')
+prettify
+xlabel('score'); ylabel('occurrence')

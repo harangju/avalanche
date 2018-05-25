8826bd456a0d7f33535963f8dcc27a6299913e0c
Harang Ju <hju@chead.uphs.upenn.edu>
2018-05-25 13:57:16 -0400

    fix some stuff

--- avalanche generation/spontaneous_avalanches.m
+++ avalanche generation/spontaneous_avalanches.m
@@ -4,12 +4,12 @@ function Y = spontaneous_avalanches(A, B, prob, iter)
 
 N = size(A,1);
 Y = zeros(N, iter);
-y_spont = rand(N, iter) < prob;
 
 zero_input = zeros(length(B),1);
-Y(:,1) = y_spont(:,1) < prob;
+Y(:,1) = rand(N,1) < prob;
 for t = 2 : iter
-    Y(:,t) = Y(:,t) + y_spont(:,t);
+    y_spont = rand(N,1) < prob;
+    Y(:,t) = Y(:,t) + y_spont;
     Y(:,t) = Y(:,t) +...
         stochastic_spike_propagation(A, B, Y(:,t-1), zero_input);
 %     Y(:,t) = double(Y(:,t) > 0);
--- cluster/simulate.m
+++ cluster/simulate.m
@@ -1,6 +1,6 @@
 %% spontaneous activity
-p_spike = 1e-5;
-iter = 1e4;
+p_spike = 1e-4;
+iter = 1e6;
 tic
 Y = spontaneous_avalanches(A,B,p_spike,iter);
 toc

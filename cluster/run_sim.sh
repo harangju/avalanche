a1227ccb2dd34d984e08f6b22350e9e9eadcae16
Harang Ju <hju@chead.uphs.upenn.edu>
2018-05-25 12:07:53 -0400

    update run_sim

--- cluster/run_sim.sh
+++ cluster/run_sim.sh
@@ -4,12 +4,16 @@
 # mail me when job ends
 #$ -M harangju@gmail.com
 
+sweep_params="$1"
+
 com=""
 com="$com cd('~/matlab');"
 com="$com addpath(genpath('avalanche'));"
 com="$com load_libs;"
 com="$com set_parameters;"
+com="$com $sweep_params;"
 com="$com initialize;"
 com="$com simulate;"
 
-matlab -nodisplay -nodesktop -r "$com"
+echo $com
+#matlab -nodisplay -nodesktop -r "$com"

d09727e30c36e40b95959df35d8342a354291bcf
Harang Ju <harangju@gmail.com>
2018-06-11 16:17:55 -0400

    diag eig analysis

--- scratchpad2.m
+++ scratchpad2.m
@@ -1,8 +1,8 @@
 
 %%
-N = 10;
-A = diag(rand(1,10));
-B = ones(10,1);
+N = 100;
+A = diag(rand(1,N)*0.9);
+B = ones(N,1);
 p.num_nodes = N;
 imagesc(A); colorbar; prettify
 %%
@@ -17,16 +17,17 @@ clf; hold on
 scatter(d(pat),duration,'.')
 scatter(d,dur_mean,'filled','r')
 lin = d.^(1:100)';
-exp_dur = zeros(1,10);
-for i = 1 : 10
+exp_dur = zeros(1,N);
+for i = 1 : N
 exp_dur(i) = find(lin(:,i)<thresh,1);
 end
 scatter(d,exp_dur,'g')
 prettify; hold off
-[d'; dur_mean; exp_dur]
+%%
+[d'; dur_mean; exp_dur; d'.^dur_mean]
 
 %%
-t = 30;
+t = 40;
 clf; hold on
 plot(d.^(0:t)','LineWidth',1.5)
 plot(1:t,thresh*ones(1,t),'LineWidth',1.5)
@@ -34,4 +35,6 @@ hold off
 prettify
 
 %%
-[d'; dur_mean; exp_dur; sqrt(d'./(1-d').^2); 1./(1-d')]
+scatter(d,d'.^dur_mean,'filled')
+prettify
+axis([0 1 0 0.06])

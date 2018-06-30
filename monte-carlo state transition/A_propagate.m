1d6cd4add1a59a7148cb2d7cc003faf10c5b3110
Jason <jinsu1@seas.upenn.edu>
2018-06-30 19:30:59 -0400

    Commented version of reasonable predictor of stimulus duration.

--- monte-carlo state transition/A_propagate.m
+++ monte-carlo state transition/A_propagate.m
@@ -3,16 +3,17 @@ function [S, pInd] = A_propagate(A, x0, T)
 % assume x0 is # trials (m) by # neurons (n)
 
 [m, n] = size(x0);
-S = zeros([m,n,T]);
-S(:,:,1) = x0;
+S = zeros([m,T,n]);
+S(:,1,:) = reshape(x0, [m,1,n]);
 
 % XR = repmat(x0, [1,1,n]);
-AR = repmat(reshape(A', [1,n,n]), [m,1,1]);
+AR = repmat(reshape(A, [1,n,n]), [m,1,1]);
 pInd = true(m,T);
 
 for i = 2:T
-    S(pInd(:,i-1),:,i) = sum(binornd(repmat(S(pInd(:,i-1),:,i-1), [1,1,n]), AR(pInd(:,i-1),:,:)),3);
-    pInd(:,i) = sum(S(:,:,i),2)>0;
+    disp(i)
+    S(pInd(:,i-1),i,:) = sum(binornd(repmat(S(pInd(:,i-1),i-1,:), [1,n,1]), AR(pInd(:,i-1),:,:)),3);
+    pInd(:,i) = sum(S(:,i,:),3)>0;
 end
 
 
--- monte-carlo state transition/main.m
+++ monte-carlo state transition/main.m
@@ -3,53 +3,67 @@ clear; clc;
 
 
 %% Parameters
-n = 15;
-m = 1;
-A = rand(n).^2;
-A = A ./ sum(A);
+n = 20;                     % Number Neurons
+m = 1;                      % 
+A = rand(n).^3;             % Adjacency Matrix
+A = A ./ sum(A);            % Normalize to get out-strength = 1
 
 
 %% Estimate transition matrix
-tic
-[P a] = MC_transition(A, m, 1000);
-toc
+% tic
+% [P a] = MC_transition(A, m, 1000);
+% toc
 
 
-%% Propagate
-% Simulation
-tic
-NT = 10000;
-T = 100;
-x0 = [ones(NT,1), zeros(NT, n-1)];
+%% Simulation
+NT = 10000;                 % Number Trials
+T = 100;                    % Number Time Steps
+xInd = 1;                   % Not important, keep at 1
+a = zeros([n,1]); 
+a([1 2 3]) = 1;             % Stimulus vector
+
+% Simulate
+x0 = repmat(a(:,xInd), [1, NT])';
 [S, pInd] = A_propagate(A, x0, T);
-toc
+pIndA = sum(pInd);          % Number neurons alive at time bins
 
-% Markov
-p0 = zeros([size(a,2),1]); p0(2) = 1;
-p = zeros([size(a,2),T]); p(:,1) = p0;
-for i = 2:T
-    p(:,i) = P * p(:,i-1);
-end
-pIndA = sum(pInd);
-toc
 
-% Extra Predictor
-x = zeros(n, T);
-x(:,1) = a(:,2);
+%% Markov
+% p0 = zeros([size(a,2),1]); p0(2) = 1;
+% p = zeros([size(a,2),T]); p(:,1) = p0;
+% for i = 2:T
+%     p(:,i) = P * p(:,i-1);
+% end
+% toc
+
+
+%% Extra Predictor
+x = zeros(n, T);            % Expected Value
+v = zeros(n, T);            % Uppder Bound on Variance
+x(:,1) = a(:,xInd);         % Initialize
+v(:,1) = a(:,xInd);
+% Compute components purely structure based predictor
 for i = 2:T
     x(:,i) = A*x(:,i-1);
+    v(:,i) = (A - A.^2)*x(:,i-1) + A*v(:,i-1);
 end
-H = -sum(x.*log2(x), 'omitnan');
+% H = -sum(x.*log2(x), 'omitnan');
+% H = sum(x ./ sqrt(v));
+H = mean(x./v, 'omitnan').^(1/sum(a(:,xInd)));  % Structural Predictor
 
 
-
+% Plot
 figure(1); clf;
 subplot(2,2,1);
-histogram(sum(P))
+% histogram(sum(P))
 subplot(2,2,2);
 loglog(-diff(pIndA));
+xlabel('Time Step');
+ylabel('Number Dead at Time Step');
 subplot(2,2,3);
-plot(p(1,:), pIndA);
+% plot(1-p(1,:), pIndA/NT);
 subplot(2,2,4);
-plot(H, pIndA);
+plot(H, pIndA/NT);
+xlabel('Structural Predictor');
+ylabel('Simulated Fraction Alive');
 

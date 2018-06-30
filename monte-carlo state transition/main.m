ebd2bd343cc11e3f0f92b2dc1a3d43fa7c9f2c12
Jason <jinsu1@seas.upenn.edu>
2018-06-30 17:21:40 -0400

    Addition of notes on the analytics of probability distributions, reconstructions, etc.

--- monte-carlo state transition/main.m
+++ monte-carlo state transition/main.m
@@ -17,6 +17,7 @@ toc
 
 %% Propagate
 % Simulation
+tic
 NT = 10000;
 T = 100;
 x0 = [ones(NT,1), zeros(NT, n-1)];
@@ -30,6 +31,16 @@ for i = 2:T
     p(:,i) = P * p(:,i-1);
 end
 pIndA = sum(pInd);
+toc
+
+% Extra Predictor
+x = zeros(n, T);
+x(:,1) = a(:,2);
+for i = 2:T
+    x(:,i) = A*x(:,i-1);
+end
+H = -sum(x.*log2(x), 'omitnan');
+
 
 
 figure(1); clf;
@@ -39,3 +50,6 @@ subplot(2,2,2);
 loglog(-diff(pIndA));
 subplot(2,2,3);
 plot(p(1,:), pIndA);
+subplot(2,2,4);
+plot(H, pIndA);
+

7c73aefb1fd70014d13e43c882deadcc5ac94a5d
Harang Ju <harangju@gmail.com>
2018-07-02 23:32:04 -0400

    fig3 updating

---
+++ paper figures/fig3_new2.m
@@ -0,0 +1,96 @@
+
+% many graph topologies
+graphs = {'WRG','RG','MD4','WS'};
+trials = 30;
+% variables
+H_m = cell(length(graphs),trials);
+mi_pops = cell(length(graphs),trials);
+max_ent = cell(length(graphs),trials);
+f = zeros(length(graphs),trials);
+% simulation parameters
+dur=40; iter=1e3; scale = 5;
+% loop
+parfor g = 1 : length(graphs)
+    for t = 1 : trials
+        disp([graphs{g} ' ' num2str(t)])
+        tic
+        % generate graph
+        p = default_network_parameters;
+        p.num_nodes = 100;
+        p.num_nodes_input = p.num_nodes;
+        p.num_nodes_output = p.num_nodes;
+        p.frac_conn = 0.1;
+        p.graph_type = graphs{g};
+        [A, B] = network_create(p);
+        A = scale_weights_to_criticality(A);
+        % stimulus patterns
+        % eigendecomposition
+        [v,d] = eig(A);
+        d = diag(d);
+        % make inputs with eigenvectors real, positive, & integers
+        pats = cell(1,p.num_nodes);
+        dups = 0;
+        for i = 1 : p.num_nodes
+            pats{i} = v(:,i);
+            if ~isreal(d(i))
+                if i < p.num_nodes &&...
+                        abs(d(i)) == abs(d(i+1))
+                    pats{i} = v(:,i) + v(:,i+1);
+                elseif abs(d(i)) == abs(d(i-1))
+                    pats{i} = v(:,i) + v(:,i-1);
+                    dups = dups + 1;
+                end
+            end
+            if find(pats{i}<0); pats{i} = -1 * pats{i}; end
+            pats{i}(pats{i}<0) = 0;
+            pats{i} = abs(pats{i});
+            pats{i} = round(scale * pats{i});
+        end
+        % remove duplicates
+        if sum(abs(pats{1} - pats{2})) > 1e-10
+            d_real = d(1);
+            pats_no_dup = pats(1);
+        else
+            d_real = [];
+            pats_no_dup = {};
+        end
+        for i = 2 : length(pats)
+            if sum(abs(pats{i-1} - pats{i})) > 1e-10
+                pats_no_dup = [pats_no_dup pats{i}];
+                d_real = [d_real abs(d(i))];
+            end
+        end
+        pats = pats_no_dup;
+        % simulation
+        mi_pops{g,t} = zeros(length(pats), dur);
+        max_ent{g,t} = zeros(1,length(pats));
+        for i = 1 : length(pats)
+%             disp([num2str(i) '/' num2str(length(pats))])
+            probs = 0.5*ones(1,length(pats))/(length(pats)-1);
+            probs(i) = 0.5;
+%             tic
+            [Y,pat] = trigger_many_avalanches(A, B, pats, probs, dur, iter);
+%             toc
+            pa = pat';
+            pa(pat==i) = 1;
+            pa(pat~=i) = 2;
+            mi_pops{g,t}(i,:) = mutual_info_pop(Y,pa);
+            max_ent{g,t}(i) = h(pa);
+        end
+        % predictor
+        H = zeros(length(pats),dur);
+        for i = 1 : length(pats)
+            H(i,:) = avalanche_predictor(A,pats{i},dur);
+        end
+        H_m{g,t} = mean(H.*(1:dur),2);
+        H_m{g,t}(isnan(H_m{g,t})) = 0;
+        toc
+        % get p values
+    end
+end
+
+
+
+
+
+

33608cd1458dff4d1c87aa50796283f42b65835d
jk6294 <jinsu1@seas.upenn.edu>
2018-07-10 21:59:55 -0400

    Some basic updates on tuning initial conditions for avalanches

--- monte-carlo state transition/main.m
+++ monte-carlo state transition/main.m
@@ -3,45 +3,45 @@ clear; clc;
 
 
 %% Parameters
-n = 100;                     % Number Neurons
+n = 10;                     % Number Neurons
 m = 1;                      % 
 A = rand(n).^3;             % Adjacency Matrix
 A = A ./ sum(A);            % Normalize to get out-strength = 1
 
 
 %% Estimate transition matrix
-% tic
-% [P a] = MC_transition(A, m, 1000);
-% toc
+tic
+[P a] = MC_transition(A, m, 1000);
+toc
 
 
 %% Simulation
 NT = 10000;                 % Number Trials
 T = 1000;                   % Number Time Steps
 xInd = 1;                   % Not important, keep at 1
-a = zeros([n,1]); 
-a([1 2 3]) = 2;             % Stimulus vector
+aC = zeros([n,1]); 
+aC([1 2 3]) = 2;             % Stimulus vector
 
 % Simulate
-x0 = repmat(a(:,xInd), [1, NT])';
+x0 = repmat(aC(:,xInd), [1, NT])';
 [S, pInd] = A_propagate(A, x0, T);
 pIndA = sum(pInd);          % Number neurons alive at time bins
 
 
 %% Markov
-% p0 = zeros([size(a,2),1]); p0(2) = 1;
-% p = zeros([size(a,2),T]); p(:,1) = p0;
-% for i = 2:T
-%     p(:,i) = P * p(:,i-1);
-% end
-% toc
+p0 = zeros([size(aC,2),1]); p0(2) = 1;
+p = zeros([size(aC,2),T]); p(:,1) = p0;
+for i = 2:T
+    p(:,i) = P * p(:,i-1);
+end
+toc
 
 
 %% Extra Predictor
 x = zeros(n, T);            % Expected Value
 v = zeros(n, T);            % Uppder Bound on Variance
-x(:,1) = a(:,xInd);         % Initialize
-v(:,1) = a(:,xInd);
+x(:,1) = aC(:,xInd);         % Initialize
+v(:,1) = aC(:,xInd);
 % Compute components purely structure based predictor
 for i = 2:T
     x(:,i) = A*x(:,i-1);
@@ -49,7 +49,7 @@ for i = 2:T
 end
 % H = -sum(x.*log2(x), 'omitnan');
 % H = sum(x ./ sqrt(v));
-H = mean(x./v, 'omitnan').^(1/sum(a(:,xInd)));  % Structural Predictor
+H = mean(x./v, 'omitnan').^(1/sum(aC(:,xInd)));  % Structural Predictor
 
 
 % Plot

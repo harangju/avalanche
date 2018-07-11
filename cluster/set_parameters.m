4894bfe3cf51f67bc444108486b195eb96ef7954
Harang Ju <harangju@gmail.com>
2018-07-11 11:34:37 -0400

    reorganize figure data

--- cluster/set_parameters.m
+++ cluster/set_parameters.m
@@ -1,6 +1,6 @@
 %% network parameters
 param = default_network_parameters;
-param.num_nodes = 100;
+param.num_nodes = 30;
 param.num_nodes_input = param.num_nodes;
 param.num_nodes_output = param.num_nodes;
 param.frac_conn = 0.05;

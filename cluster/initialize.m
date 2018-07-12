849d6f744ce736a16da6d5417e63c1807a99bc47
Harang Ju <harangju@gmail.com>
2018-07-12 15:55:36 -0400

    randomize weights

--- cluster/initialize.m
+++ cluster/initialize.m
@@ -1,6 +1,7 @@
 %% set up environment
-% addpath(genpath('..'))
-% addpath(genpath('../../wu-yan-2018-code'))
+addpath(genpath('..'))
+addpath(genpath('../../wu-yan-2018-code'))
 %% initialize network
 [A, B, C] = network_create(param);
+A = A .* rand(size(A));
 A = scale_weights_to_criticality(A);

a266796cfc1fe10894219ec4cf53d2ca4791c3c0
Harang Ju <harangju@gmail.com>
2018-07-13 12:11:32 -0400

    ws cluster

--- cluster/set_parameters.m
+++ cluster/set_parameters.m
@@ -1,8 +1,16 @@
 %% network parameters
-N = [10 10 10];
-frac_conn = [1 1 1];
-param.num_nodes = sum(N);
-p_rewire = 0;
+param = default_network_parameters;
+param.num_nodes = 30;
+param.num_nodes_input = param.num_nodes;
+param.num_nodes_output = param.num_nodes;
+param.frac_conn = 0.05;
+param.graph_type = 'WS';
+param.p_rewire = 1;
+
+% N = [10 10 10];
+% frac_conn = [1 1 1];
+% param.num_nodes = sum(N);
+% p_rewire = 0;
 
 % p_spike = 1e-4;
 dur = 1e3;

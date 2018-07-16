cba33888922c59c778dcce775586541eede4d2b1
Harang Ju <harangju@gmail.com>
2018-07-16 19:35:00 -0400

    scratchpads

--- scratchpad.m
+++ scratchpad.m
@@ -13,7 +13,9 @@ for i = 1 : N
     pats{i}(i) = 1;
 end
 
-transitions = 0.05 : 0.05 : 0.95;
+% transitions = 0.05 : 0.05 : 0.95;
+% transitions = 0.01 : 0.01 : 0.1;
+transitions = 0.02 : 0.02 : 0.1;
 slopes = zeros(size(transitions));
 shifts = zeros(size(transitions));
 xs = cell(size(slopes));
@@ -25,9 +27,11 @@ parfor i = 1 : length(transitions)
     A = A0;
     A(1,2) = A(1,2) - transitions(i);
     A(1,1) = transitions(i);
+    A(2,1) = A(2,1) - transitions(i);
+    A(2,2) = transitions(i);
     As{i} = A;
     % simulate
-    dur = 1e3; iter = 1e4;
+    dur = 1e3; iter = 6e3;
     probs = ones(1,length(pats)) / length(pats);
     tic
     [Y,pat] = trigger_many_avalanches(A,B,pats,probs,dur,iter);
@@ -51,15 +55,29 @@ parfor i = 1 : length(transitions)
     y(y==0) = [];
     xs{i} = x;
     ys{i} = y;
-    f = polyfit(x,y,1);
+    f = polyfit(x(1:15),y(1:15),1);
     slopes(i) = f(1);
     shifts(i) = f(2);
 end
+
+%% plot
+scatter(transitions, shifts, 'filled')
+xlabel('autapse strength')
+ylabel('shifts')
+axis([0 1 1 2])
+yyaxis right
+scatter(transitions, slopes, 'filled')
+ylabel('slopes')
+axis([0 1 -2 -1])
+prettify
 
 %% plot
 for i = 1 : length(transitions)
     scatter(xs{i}, ys{i}, '.')
     axis([0 3 -4 0])
     prettify; title(num2str(transitions(i)))
+    hold on
+    plot(xs{i},polyval([slopes(i) shifts(i)], xs{i}))
+    hold off
     pause
 end
--- scratchpad3.m
+++ scratchpad3.m
@@ -4,7 +4,7 @@ param = default_network_parameters;
 param.num_nodes = 30;
 param.num_nodes_input = param.num_nodes;
 param.num_nodes_output = param.num_nodes;
-param.frac_conn = 0.05;
+param.frac_conn = 0.1;
 param.graph_type = 'WRG';
 param.p_rewire = 1;
 

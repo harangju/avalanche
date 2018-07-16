29428ad8691bc851ff7bc95410ed99bd0208f1cd
Harang Ju <harangju@gmail.com>
2018-07-16 14:32:25 -0400

    two node cycles

--- cluster/set_parameters.m
+++ cluster/set_parameters.m
@@ -3,7 +3,7 @@ param = default_network_parameters;
 param.num_nodes = 30;
 param.num_nodes_input = param.num_nodes;
 param.num_nodes_output = param.num_nodes;
-param.frac_conn = 0.05;
+param.frac_conn = 0.2;
 param.graph_type = 'WS';
 param.p_rewire = 1;
 

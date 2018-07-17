e55bd064ecc6dfdb0ac695f5546c725db9893721
Harang Ju <harangju@gmail.com>
2018-07-17 13:52:37 -0400

    cluster cycles

--- cluster/set_parameters.m
+++ cluster/set_parameters.m
@@ -1,18 +1,24 @@
 %% network parameters
-param = default_network_parameters;
-param.num_nodes = 30;
-param.num_nodes_input = param.num_nodes;
-param.num_nodes_output = param.num_nodes;
-param.frac_conn = 0.2;
-param.graph_type = 'WS';
-param.p_rewire = 1;
+% param = default_network_parameters;
+% param.num_nodes = 30;
+% param.num_nodes_input = param.num_nodes;
+% param.num_nodes_output = param.num_nodes;
+% param.frac_conn = 0.2;
+% param.graph_type = 'WS';
+% param.p_rewire = 1;
 
 % N = [10 10 10];
 % frac_conn = [1 1 1];
 % param.num_nodes = sum(N);
 % p_rewire = 0;
 
+% set parameters
+A0 = [0 1; 1 0]';
+B = [1 1]';
+N = 2;
+redistr = 0.1;
+
 % p_spike = 1e-4;
 dur = 1e3;
-iter = 1e4;
-
+% iter = 1e4;
+iter = 3e3;
--- cluster/simulate.m
+++ cluster/simulate.m
@@ -1,27 +1,13 @@
 
 %% stimulus
-pats = cell(1,param.num_nodes);
-for i = 1 : param.num_nodes
-    pats{i} = zeros(param.num_nodes,1);
+pats = cell(1,N);
+for i = 1 : N
+    pats{i} = zeros(N,1);
     pats{i}(i) = 1;
 end
 
-%%
+%% simulate
 probs = ones(1,length(pats)) / length(pats);
 tic
 [Y,pat] = trigger_many_avalanches(A,B,pats,probs,dur,iter);
 toc
-
-%%
-activity = squeeze(sum(Y,1))';
-durations = zeros(1,iter);
-for i = 1 : iter
-    if sum(activity(i,:)) > 0
-        durations(i) = find(activity(i,:)>0,1,'last');
-    else
-        durations(i) = 0;
-    end
-end; clear i
-
-%%
-save

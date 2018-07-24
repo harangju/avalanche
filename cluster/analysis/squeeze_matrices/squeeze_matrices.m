4f3ac4b3b01076a07fd621955425c9c9b6accece
harangju <harangju@gmail.com>
2018-07-24 14:19:32 -0400

    update squeeze

--- cluster/analysis/squeeze_matrices/squeeze_matrices.m
+++ cluster/analysis/squeeze_matrices/squeeze_matrices.m
@@ -3,11 +3,12 @@
 
 subdirs = dir(result_dir);
 for d = 3 : length(subdirs)
-    load([result_dir '/' subdirs(d).name '/matlab.mat'])
+%     load([result_dir '/' subdirs(d).name '/matlab.mat'])
+    load([subdirs(d).name '/matlab.mat'])
     Yc = cell(1,size(Y,3));
     for i = 1 : size(Y,3)
         Yc{i} = sparse(Y(:,:,i));
     end
     clear Y
-    save([result_dir '/' subdirs(d).name '/matlab.mat'],'-v7.3')
+    save([subdirs(d).name '/matlab_cmpr.mat'],'-v7.3')
 end
--- cluster/analysis/squeeze_matrices/squeeze_matrices.sh
+++ cluster/analysis/squeeze_matrices/squeeze_matrices.sh
@@ -6,9 +6,11 @@
 
 dir_name="$1"
 
+cd ~/results/$dir_name
+
 com=""
 com="$com result_dir = '~/results/$dir_name';"
-com="$com addpath('~/matlab/avalanche/cluster/squeeze_matrices')"
+com="$com addpath('~/matlab/avalanche/cluster/analysis/squeeze_matrices');"
 com="$com squeeze_matrices"
 #dir=`pwd`
 #com="$com save $dir/matlab_cmprs.mat -v7.3"

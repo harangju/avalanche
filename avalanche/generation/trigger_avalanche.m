074d5897da80bfe86abe3f7fa3498b3b21d6d670
Harang Ju <harangju@gmail.com>
2018-08-03 16:29:53 -0400

    update avalanche triggering

--- avalanche/generation/trigger_avalanche.m
+++ avalanche/generation/trigger_avalanche.m
@@ -1,5 +1,5 @@
 function Y_t = trigger_avalanche(A, B, u_t, max_duration)
-%trigger_avalanche
+%trigger_avalanche - vectorized
 %   A: system connectivity, [pre X post]
 %   B: system input connectivity, [input X N]
 %   u_t: input to system over time t, [N X t]
@@ -12,18 +12,11 @@ Y_t = zeros(N, max_duration); % firing
 u_t = padarray(u_t, [0 max_duration-size(u_t,2)], 'post');
 % add zero padding to u_t
 
-for t = 1 : max_duration
-    for j = 1 : N % firing Y, neuron i -> neuron j
-        spikes_u = sum(rand(u_t(j,t),1) < B(j));
-        spikes_Y_j = 0;
-        if t > 1
-            for i = 1 : N
-                spikes_Y_j = spikes_Y_j + sum(rand(Y_t(i,t-1),1) < A(i,j));
-            end
-        end
-        Y_t(j,t) = Y_t(j,t) + spikes_u + spikes_Y_j;
-    end
-    if sum(Y_t(:,t)) == 0 && t >= size(u_t,2); break; end
+spikes_u = binornd(u_t(:,1), B);
+Y_t(:,1) = spikes_u;
+for t = 2 : max_duration
+    Y_t(:,t) = stochastic_spike_propagation(A,B,Y_t(:,t-1),u_t(:,t));
+    if sum(Y_t(:,t)) == 0; break; end
 end
 Y_t = Y_t(:,1:t);
 

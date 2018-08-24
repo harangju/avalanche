fc0c289815108ef2e1bf33c433cc915eb68f9662
Harang Ju <harangju@gmail.com>
2018-08-23 20:47:14 -0400

    avl_size

--- avalanche/analysis/avl_sizes.m
+++ avalanche/analysis/avl_sizes.m
@@ -1,4 +1,4 @@
-function s = avl_size(Y)
+function s = avl_sizes(Y)
 %avalanche_size Calculates avalanche size given an avalanche Y (without
 %without triggering any avalanches)
 %   Y: [N X duration] matrix, avalanche
---
+++ avalanche/analysis/avl_sizes_cell.m
@@ -0,0 +1,15 @@
+function s = avl_sizes_cell(Y)
+% AVL_SIZE_CELL
+%
+%   AVL_SIZE_CELL(Y)
+%
+%   Example:
+%       Y = trigger_
+
+num_trials = length(Y);
+s = zeros(1,num_trials);
+for i = 1 : num_trials
+    s(i) = avl_sizes(Y{i});
+end
+
+end

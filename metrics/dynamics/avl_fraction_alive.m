3fc8d9338c711ecd4267fc2c47a41f10ccdd857b
Harang Ju <harangju@gmail.com>
2018-10-18 15:19:43 -0400

    rename

--- metrics/dynamics/avl_fraction_alive.m
+++ metrics/dynamics/avl_fraction_alive.m
@@ -1,4 +1,4 @@
-function f = fraction_alive(Y)
+function f = avl_fraction_alive(Y)
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
 
--- paper figures/fig2.m
+++ paper figures/fig2.m
@@ -6,4 +6,7 @@ trials = 1e5;
 n = 8;
 [pats, probs] = pings_single(p.N);
 Y = avalanche_smp_many(pats{n},1,A,dur,trials);
+d = avl_durations_cell(Y);
+f = avl_fraction_alive();
+%%
 

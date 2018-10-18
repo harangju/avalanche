b223eb91bdbade1408f65f1d9b47def8ae85d5df
Harang Ju <harangju@gmail.com>
2018-10-18 11:06:37 -0400

    generate binary states

--- metrics/probability/p_transition.m
+++ metrics/probability/p_transition.m
@@ -2,12 +2,18 @@ function ptrans = p_transition(A, states)
 %p_transition
 %
 %   Parameters:
-%       A: (N-by-2^N)
+%       A: (N-by-N)
+%       states: (N-by-2^N)
 %
 %   Results:
 %       
 %
 %   Example:
 %       
+
+Sk = repmat(states, []);
+Sl = ;
+
+ptrans = prod(1 - Sk - A' * Sl, 1);
 
 end

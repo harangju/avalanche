1bd71e9b99a2bbe667933cfd0e04f111e72a7c3c
Harang Ju <harangju@gmail.com>
2018-10-29 10:59:41 -0400

    fig3 update

--- paper figures/fig3_3.m
+++ paper figures/fig3_3.m
@@ -1,15 +1,12 @@
 
 %% load distributions
-% result_dir = 'avalanche/paper data/20180717_140743';
-% result_dir = 'avalanche/paper data/20180718_130604';
-% result_dir = 'avalanche/paper data/20180730_174425';
-result_dir = '~/Desktop/20180806_124849';
+result_dir = '~/Desktop/20181026_153621'; % 4-cycle, seed = 2
+% result_dir = '~/Desktop/20181029_104029'; % 4-cycle, seed = 2
 subdirs = dir(result_dir);
 
 durs = cell(1,length(subdirs)-2);
 dur_max = zeros(1,length(subdirs)-2);
 distr = zeros(1,length(subdirs)-2);
-vars = cell(1,length(subdirs)-2);
 As = cell(1,length(subdirs)-2);
 patses = cell(1,length(subdirs)-2);
 
@@ -20,17 +17,10 @@ for d = 3 : length(subdirs)
     As{d-2} = A;
     dur_max(d-2) = dur;
     patses{d-2} = pats;
-    durations = cellfun(@length,Y) - 1;
-    durs{d-2} = durations;
-    % variance
-%     v = zeros(size(Y,1), dur);
-%     for t = 1 : dur
-%         v(:,t) = var(Y(:,t,:),0,3);
-%     end
-%     vars{d-2} = v;
+    durs{d-2} = avl_durations_cell(Y);
 end
 
-clearvars -except durs dur_max distr vars As patses
+clearvars -except durs dur_max distr vars As order
 
 %% calculate histogram counts
 
@@ -40,15 +30,9 @@ ys = cell(1,length(durs));
 bin_count = 1e5;
 
 for i = 1 : length(xs)
-    [c_d,e_d] = histcounts(durs{i},bin_count);
-    x = log10(e_d(2:end));
-    y = log10(c_d/sum(c_d));
-    x(isinf(y)) = [];
-    y(isinf(y)) = [];
-    xs{i} = x;
-    ys{i} = y;
+    [xs{i}, ys{i}] = hist_log10(durs{i}, bin_count);
 end
-clear bin_count c_d e_d x y i
+clear bin_count
 
 %% calculate - slope, intercepts of dur distr
 slopes = zeros(1,length(xs));
@@ -56,12 +40,13 @@ y_int = zeros(1,length(xs));
 x_int = zeros(1,length(xs));
 % pts = 10:30;
 for i = 1 : length(distr)
-    if i == 1 || i == length(distr)
-        pts = 20:40;
-        pts = 30:50;
-    else
-        pts = 10:30;
-    end
+%     if i == 1 || i == length(distr)
+%         pts = 20:40;
+%         pts = 30:50;
+%     else
+%         pts = 10:30;
+%     end
+    pts = 30:50;
     f = polyfit(xs{i}(pts), ys{i}(pts), 1);
     slopes(i) = f(1);
     y_int(i) = f(2);
@@ -70,7 +55,7 @@ end
 clear i f
 
 %% plots - example distribution
-idx_distr = 1 : 4 : length(xs)/2+1;
+idx_distr = 1 : 11 : length(xs)/2+1;
 % idx_distr = 1 : 11 : length(xs)/2+1;
 % colors = linspecer(length(idx_distr));
 colors = [3.1, 18.8, 42;...
@@ -89,14 +74,14 @@ for i = 1 : length(idx_distr)
 end
 prettify
 % axis([0 3.1 -5 0])
-legend(plts,lgnd,'Location','Southwest')
+legend(plts,lgnd,'Location','Northeast')
 clear idx_distr colors plts lgnd i idx
 
 %% plots - delta w vs slopes
 clf
 scatter(distr,slopes,20,[3.1, 18.8, 42]./100,'filled')
 prettify
-axis([-.1 1.1 -3 0])
+% axis([-.1 1.1 -3 0])
 hold on
 f_dur = polyfit(distr,slopes,2);
 plot(distr,polyval(f_dur,distr),'Color',[3.1 18.8 42]./100)

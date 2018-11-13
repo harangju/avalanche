640a4d4a3cfcbaa8a9b508ac2a81e7ebfcf82e2a
Harang Ju <harangju@gmail.com>
2018-11-13 14:47:30 -0500

    refix mutual info pop

--- metrics/information/mutual_info_pop.m
+++ metrics/information/mutual_info_pop.m
@@ -5,13 +5,11 @@ function [mi_pop, code] = mutual_info_pop(Y,pat)
 %returns
 %   mi: [1 by duration]
 
-T = length(pat);
-K = size(Y,3);
-code = zeros(K,T);
-code(:,1:size(Y,2)) = pop_code_by_step(Y);
+T = size(Y,2);
+code = pop_code_by_step(Y);
 mi_pop = zeros(1,T);
-for i = 1 : T
-    mi_pop(i) = mi(pat,code(:,i));
+for t = 1 : T
+    mi_pop(i) = mi(pat,code(:,t));
 end
 
 end

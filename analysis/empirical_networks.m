051d79d9f47b10182e91fcf8191d301b779f6def
Harang Ju <wngkfkd94@gmail.com>
2019-04-03 11:35:16 -0400

    update emp net calc

--- analysis/empirical_networks.m
+++ analysis/empirical_networks.m
@@ -3,12 +3,14 @@ data_dir = 'beggs data';
 files = dir([data_dir '/*.mat']);
 bin_size = 4;
 ns = cell(1,length(files));
-for i = 7:-1:1 %length(files):-1:1
+for i = length(files):-1:1
     disp(['Analyzing ' files(i).name '...'])
     load([data_dir '/' files(i).name]);
-    ns{i} = net.generate('topology','autoregressive',...
+    ns{i} = net.generate('autoregressive',...
         'v',spike_times_to_bins(data.spikes,bin_size)',...
         'pmin',1,'pmax',4);
     ns{i}.v = [];
+    save('emp_net_bs4')
 end; clear i data
+save('emp_net_bs4')
 %% 

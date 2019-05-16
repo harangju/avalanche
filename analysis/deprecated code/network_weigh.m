2520013f8a51af3d87730c039b6cd05766cd2b2e
Harang Ju <wngkfkd94@gmail.com>
2019-05-16 15:08:32 -0400

    fig4 done

---
+++ analysis/deprecated code/network_weigh.m
@@ -0,0 +1,33 @@
+function A = network_weigh(A, type, params, weigh_by_neuron)
+%network_weigh Returns a weighted network
+%   called by network_create
+%
+%   Example:
+%       A = network_weigh(A, 'uniform', 1);
+%       A = network_weigh(A, 'gaussian', [0.5 0.15]);
+%       A = network_weigh(A, 'bimodalgaussian', ...
+%           [0.1 0.9 0.9 0.1 0.1]);
+%       A = network_weigh(A, 'powerlaw', 3);
+%
+%   See also network_create, network_connect
+
+if strcmp(type, 'none')
+    return
+end
+
+if weigh_by_neuron
+    N = size(A,1);
+    for n = 1 : N
+        weights = A(n,:);
+        [~,idx_sort] = sort(weights(weights>0));
+        new_weights = distribution(type, length(idx_sort), params);
+        idx_pos = find(weights);
+        A(n,idx_pos(idx_sort)) = new_weights;
+    end
+else
+    weights = A(:);
+    [~,idx_sort] = sort(weights(weights>0));
+    new_weights = distribution(type, length(idx_sort), params);
+    idx_pos = find(weights);
+    A(idx_pos(idx_sort)) = new_weights;
+end
--- figures/fig4_abcdef.m
+++ figures/fig4_abcdef.m
@@ -1,49 +1,51 @@
-
-%% network
-% NEED TO UPDATE TO USE NETWORK-GENERATOR
-rng(1)
-p = default_network_parameters;
-p.weighting = 'bimodalgaussian';
-p.weighting_params = [0.1 0.9 0.9 0.1 0.1];
-% p.weighting = 'uniform';
-% p.weighting_params = 1;
-% p.weighting = 'gaussian';
-% p.weighting_params = [0.5 0.1];
-p.N = 100;
-p.frac_conn = 0.2;
-A = network_create(p);
-y0s = pings_single(p.N);
-y0sm = cell2mat(y0s);
-%% simulate
-Ys = cell(1,length(y0s));
-T = 1e3;
-K = 1e3;
-disp('Simulate cascades...')
-disp(repmat('#',[1 length(y0s)]))
-for i = 1 : length(y0s)
-    fprintf('.')
-    Ys{i} = avl_smp_many({y0s{i}},1,A,T,K);
-end; clear i; fprintf('\n')
-%% durations
-dur = cell(1,length(y0s));
-dm = zeros(1,length(y0s));
-for i = 1 : length(y0s)
-    dur{i} = avl_durations_cell(Ys{i});
-    dm(i) = mean(dur{i});
-end; clear i
-%% prediction
-[v,d] = eig(A');
-sumeig = sum(abs((v\y0sm) .* diag(d)));
-domeig = max(abs((v\y0sm) .* diag(d)));
-%% controllability
-finite_time = 100;
-ac = finite_impulse_responses(A',finite_time);
-mc = control_modal(A');
+%% try loading pre-generated data
+if exist('source_data_dir','var')
+    load([source_data_dir '/fig4_abc.mat'])
+else
+    % create network
+    n = net.generate('erdosrenyi','n',100,'p',.2,'dir',true);
+    % UNCOMMENT for figures d, e, f
+%     n = net.distr_weights(n.A,'skewbinorm',...
+%         'mus',[.1 .9],'sigma',.001,'props',[.9 .1],'range',[0 1]);
+    n.A = n.A ./ sum(n.A,2);
+    % simulate
+    y0s = pings_single(size(n.A,1));
+    y0sm = cell2mat(y0s);
+    Ys = cell(1,length(y0s));
+    T = 1e3;
+    K = 1e3;
+    disp('Simulate cascades...')
+    disp(repmat('#',[1 length(y0s)]))
+    for i = 1 : length(y0s)
+        fprintf('.')
+        Ys{i} = simulate(@smp,n.A,y0s(i),T,1,K);
+    end
+    clear i
+    fprintf('\n')
+    % durations
+    durs = cellfun(@csc_durations,Ys,'uniformoutput',0);
+    durm = cellfun(@mean,durs);
+    % eigenprojection
+    [v,d] = eig(n.A);
+    sumeig = sum(abs((v\y0sm) .* diag(d)));
+    clear v d
+    % controllability
+    F = 100;
+    fac = finite_impulse_responses(n.A,F);
+    mc = control_modal(n.A);
+    % correlations
+    [r_se,pval_se] = corr(sumeig',durm');
+    [r_mc,pval_mc] = corr(mc,durm');
+    [r_ac,pval_ac] = corr(fac,durm');
+    fprintf(['\tSE\tMC\tAC\nCorr:\t%.4f\t%.4f\t%.4f\n'...
+        'p-val:\t%.2g\t%.2g\t%.2g\n'],...
+        r_se,r_mc,r_ac,pval_se,pval_mc,pval_ac);
+end
 %% fig4a,d
 figure
 hold on
-scatter(sumeig,dm,32,[3.1, 18.8, 42]./100,'.')
-f = polyfit(sumeig',dm',1);
+plot(sumeig,durm,'k.')
+f = polyfit(sumeig',durm',1);
 x = min(sumeig) : 1e-2 : max(sumeig);
 plot(x,polyval(f,x),'r')
 prettify
@@ -51,8 +53,8 @@ title('fig4a,d')
 %% fig4b,e
 figure
 hold on
-scatter(mc,dm,32,[3.1, 18.8, 42]./100,'.')
-f = polyfit(mc,dm',1);
+plot(mc,durm,'k.')
+f = polyfit(mc,durm',1);
 x = min(mc) : 1e-2 : max(mc);
 plot(x,polyval(f,x),'r')
 prettify
@@ -60,16 +62,10 @@ title('fig4b,e');
 %% fig4c,f
 figure
 hold on
-scatter(ac,dm,32,[3.1, 18.8, 42]./100,'.')
-f = polyfit(ac,dm',1);
-x = min(ac) : 1e-2 : max(ac);
+plot(fac,durm,'k.')
+f = polyfit(fac,durm',1);
+x = min(fac) : 1e-2 : max(fac);
 plot(x,polyval(f,x),'r')
 prettify
 title('fig4c,f')
-%% correlations
-[c_ac,pval_ac] = corr(ac,dm');
-[c_se,pval_se] = corr(sumeig',dm');
-[c_mc,pval_mc] = corr(mc,dm');
-fprintf(['\tSE\tMC\tAC\nCorr:\t%.4f\t%.4f\t%.4f\n'...
-    'p-val:\t%.2g\t%.2g\t%.2g\n'],...
-    c_se,c_mc,c_ac,pval_se,pval_mc,pval_ac);
+clear f x

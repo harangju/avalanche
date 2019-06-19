20521ccbb46fceb67a5fe1a667e6cb67246c059d
Harang Ju <wngkfkd94@gmail.com>
2019-06-19 10:44:34 -0400

    Create sfig8_cluster.m

---
+++ figures/supplement/sfig8_cluster.m
@@ -0,0 +1,20 @@
+% adopted from Neural Complexity Toolbox
+%
+% Before running,
+% set variable emp_data_dir (e.g., emp_data_dir = '/Users/name/data';)
+% set variable idx_data (e.g., idx_data = 1;)
+
+%% load data
+bin_width = 5;
+disp(['Loading ' emp_data_dir '/Dataset' num2str(idx_data) '.mat...'])
+x = load([emp_data_dir '"/Dataset' num2str(idx_data) '.mat"']);
+data.raster = bin_spike_times(x.data.spikes, bin_width);
+Av = avprops(data, 'ratio', 'fingerprint');
+clear x data
+%% calculate power laws
+[tau, sz_min, sz_max, sigma_tau, p_tau, pcrit_tau] =...
+    avpropvals(Av.size, 'size');
+[alpha, dr_min, dr_max, sigma_alpha, p_alpha, pcrit_alpha] =...
+    avpropvals(Av.duration, 'duration');
+[snz, ~, ~, sigma_snz] = avpropvals({Av.size, Av.duration},...
+    'sizgivdur', 'durmin', dr_min, 'durmax', dr_max);

d22caad80873610a00d5fe986ac98e3411ce035a
Harang Ju <wngkfkd94@gmail.com>
2019-07-08 11:11:28 -0400

    consolidate sfig8

--- figures/supplement/sfig8.m
+++ figures/supplement/sfig8.m
@@ -1,44 +1,58 @@
 % adopted from Neural Complexity Toolbox
 
 %% load data
-% files = dir([emp_data_dir '/*.mat']);
-% Av = cell(length(files),1);
-% for i = 1 : length(files)
-%     disp(['Loading ' files(i).name '...'])
-%     x = load([emp_data_dir '/' files(i).name]);
-%     data.raster = bin_spike_times(x.data.spikes, 5);
-%     Av{i} = avprops(data, 'ratio', 'fingerprint');
-% end
-% clear i x data
-%% calculate power laws
-pthresh = 0.2;
-sz = cellfun(@(x) x.size, Av, 'un', 0);
-dr = cellfun(@(x) x.duration, Av, 'un', 0);
-%% variables
-tau = cell(length(sz),1);
-sz_min = cell(length(sz),1);
-sz_max = cell(length(sz),1);
-sigma_tau = cell(length(sz),1);
-p_tau = cell(length(sz),1);
-pcrit_tau = cell(length(sz),1);
-alpha = cell(length(sz),1);
-dr_min = cell(length(sz),1);
-dr_max = cell(length(sz),1);
-sigma_alpha = cell(length(sz),1);
-p_alpha = cell(length(sz),1);
-pcrit_alpha = cell(length(sz),1);
-snz = cell(length(sz),1);
-sigma_snz = cell(length(sz),1);
-%% run
-for i = 1 : length(sz)
-    disp(['Recording ' num2str(i) '...'])
-    [tau{i}, sz_min{i}, sz_max{i}, sigma_tau{i}, p_tau{i}, pcrit_tau{i}]...
-        = avpropvals(sz, 'size', 'threshold', pthresh);
-    [alpha{i}, dr_min{i}, dr_max{i}, sigma_alpha{i}, p_alpha{i}, ...
-        pcrit_alpha{i}] = avpropvals(dr, 'duration', 'threshold', pthresh);
-    [snz{i}, ~, ~, sigma_snz{i}] = avpropvals({sz{i}, dr{i}},...
-        'sizgivdur', 'durmin', dr_min{i}{1}, 'durmax', dr_max{i}{1},...
-        'threshold', pthresh);
+cascades = load_cascades(emp_data_dir);
+%% calculate size duration size|duration
+drs = cellfun(@csc_durations,cascades,'un',0);
+szs = cellfun(@csc_sizes,cascades,'un',0);
+%%
+save('stats','szs','drs')
+disp(['Run sfig8.ipynb in jupyter notebook for '...
+        'power laws. Load ft_pl. Then run next section in the script.'])
+%%
+ft_pl = table(...
+    ft_pl(:,1), ft_pl(:,2), ft_pl(:,3),...
+    'VariableNames',{'tau','alpha','xmin'});
+%%
+sgd = zeros(length(drs),1);
+sgd_inv = zeros(length(drs),1);
+sgd_log_coeff = zeros(length(drs),1);
+for i = 1 : length(drs)
+    [sgd(i), sgd_inv(i), sgd_log_coeff(i)] =...
+        sizegivdurwls(szs{i}, drs{i},...
+        'durmin', ft_pl.xmin(i), 'durmax', max(drs{i}));
+end
+clear i
+%%
+exprel = (ft_pl.alpha-1)./(ft_pl.tau-1);
+%% figure
+figure
+clf
+hold on
+plot(1:length(drs), exprel, '+')
+for i = 1 : length(sgd)
+    errorbar(i, sgd(i), sgd_inv(i), 'r')
 end
 clear i
 %% figure
+color = linspecer(3);
+% figure
+clf
+for i = 1 : length(drs)
+    subplot(5,5,i)
+    drs_above = drs{i}(drs{i}>ft_pl.xmin(i));
+    szs_above = szs{i}(drs{i}>ft_pl.xmin(i));
+    drs_below = drs{i}(drs{i}<ft_pl.xmin(i));
+    szs_below = szs{i}(drs{i}<ft_pl.xmin(i));
+    x = unique(drs_above);
+    ysgd = x.^sgd(i) * 10^sgd_log_coeff(i);
+    yexp = x.^exprel(i) * 10^sgd_log_coeff(i);
+    loglog(drs_above,szs_above,'k.','MarkerSize',6)
+    hold on
+    loglog(drs_below,szs_below,...
+        '.','MarkerSize',6,'Color',color(3,:))
+    loglog(x,ysgd,'Color',color(1,:),'LineWidth',1.5)
+    loglog(x,yexp,'--','Color',color(2,:),'LineWidth',1.5)
+    prettify
+end
+clear i x drs_* szs_*x y* ci

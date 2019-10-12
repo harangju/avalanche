09d24b082bb43f1130496e8ac7f7d86c920f10ee
Harang Ju <wngkfkd94@gmail.com>
2019-10-12 11:18:41 -0400

    sfig9

--- analysis/cascades/load_cascades.m
+++ analysis/cascades/load_cascades.m
@@ -1,4 +1,4 @@
-function [cascades, files] = load_cascades(emp_data_dir)
+function [cascades, spikes, files] = load_cascades(emp_data_dir)
 % cascades = load_cascades(emp_data_dir)
 %   returns a cell array of cascade from empirical data (see README)
 
@@ -6,10 +6,11 @@ bin_size = 5;
 
 files = dir([emp_data_dir '/*.mat']);
 cascades = cell(length(files),1);
+spikes = cell(length(files),1);
 for i = 1 : length(files)
     disp(['Analyzing ' files(i).name '...'])
     x = load([emp_data_dir '/' files(i).name]);
     data = x.data;
-    cascades{i} = detect_cascades(...
-        spike_times_to_bins(data.spikes,bin_size));
+    spikes{i} = spike_times_to_bins(data.spikes, bin_size);
+    cascades{i} = detect_cascades(spikes{i});
 end
